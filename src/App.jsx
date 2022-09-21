e3506d91f147a364d8d431ed97826537de16be92
Diego <davc93@gmail.com>
2022-09-21 13:46:17 -0300

    insertando botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import {Button} from '@mui/material'
 
 function App() {
   return (
-    <h1>App</h1>
+    <div className="buttons">
+      <Button variant='text'>Boton</Button>
+      <Button variant='contained' disabled>Boton</Button>
+      <Button variant='outlined'>Boton</Button>
+      
+    </div>
   );
 }
 
